3220950d89a151a155d453479db169fdded1dc02
kyutae_kim <kendm1@naver.com>
2026-03-03 16:04:42 +0900

    26.03.03

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,8 @@ export default function Home() {
           width={100}
           height={20}
           priority
+
+
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
